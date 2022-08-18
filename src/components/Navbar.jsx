7dc2e94b84acaa765e43e9bdbc62351a88240606
Andrei <catrina.andrei93@yahoo.com>
2022-08-18 23:50:18 +0300

    Navbar resposive design + styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,42 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { links } from "../data.js";
 import { HiMenu } from "react-icons/hi";
+import { AiOutlineCloseSquare } from "react-icons/ai";
 
 import Logo from "../images/logo.png";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false);
+
   return (
     <nav>
       <div className="container nav__container">
         <Link to="/" className="logo">
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        <ul className="nav__links">
+        <ul
+          className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}
+        >
           {links.map(({ name, path }, index) => {
             return (
               <li>
-                <NavLink to={path}>{name}</NavLink>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? "active-nav" : "")}
+                >
+                  {name}
+                </NavLink>
               </li>
             );
           })}
         </ul>
-        <button className="nav__toggle-btn">
-          <HiMenu />
+        <button
+          className="nav__toggle-btn"
+          onClick={() => setIsNavShowing(!isNavShowing)}
+        >
+          {isNavShowing ? <AiOutlineCloseSquare /> : <HiMenu />}
         </button>
       </div>
     </nav>
